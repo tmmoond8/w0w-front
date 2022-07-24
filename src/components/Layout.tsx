9dc02c0b98e02ef8b7fb88da4d59f03ab21e2955
taemin moon <tmmoond8@gmail.com>
2022-07-24 11:49:04 +0900

    ✨ Chakra UI 셋팅

---
+++ src/components/Layout.tsx
@@ -0,0 +1,28 @@
+import { Container, Box } from '@chakra-ui/react';
+
+interface Props {
+  children: React.ReactNode;
+}
+
+export default function Layout({ children }: Props) {
+  return (
+    <Box
+      display="flex"
+      flexDirection="column"
+      position="fixed"
+      left="0"
+      right="0"
+      top="0"
+      bottom="0"
+      margin="auto"
+      w="100vw"
+      h="100%"
+      minH="600px"
+      maxW="460px"
+      maxH="1080"
+      bgColor="darkcyan"
+    >
+      {children}
+    </Box>
+  );
+}
