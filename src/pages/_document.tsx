a577678e815d825ac885a10158fa06f5de80652c
taemin moon <tmmoond8@gmail.com>
2022-07-27 20:51:09 +0900

    📈 ga 추가

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import Document, { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
 
 export default class MyDocument extends Document {
   render() {
@@ -9,6 +10,26 @@ export default class MyDocument extends Document {
             href="https://fonts.googleapis.com/css?family=Gamja+Flower:400"
             rel="stylesheet"
           />
+          <Script
+            async
+            src="https://www.googletagmanager.com/gtag/js?id=G-2V7C7VZQ6C"
+          ></Script>
+          <Script
+            strategy="afterInteractive"
+            dangerouslySetInnerHTML={{
+              __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){
+              if (window.dataLayer) {
+                window.dataLayer.push(arguments);
+              }
+            }
+            gtag('js', new Date());
+
+            gtag('config', 'G-2V7C7VZQ6C');
+            `,
+            }}
+          ></Script>
         </Head>
         <body>
           <Main />
